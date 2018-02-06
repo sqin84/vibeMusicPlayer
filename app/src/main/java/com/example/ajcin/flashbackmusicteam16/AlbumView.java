81fd57bee570db618d5c5123b00a463db406f27f
ajc19 <ajcintron19@gmail.com>
2018-02-06 14:48:35 -0800

    Working build with nav bar

--- app/src/main/java/com/example/ajcin/flashbackmusicteam16/AlbumView.java
+++ app/src/main/java/com/example/ajcin/flashbackmusicteam16/AlbumView.java
@@ -17,14 +17,14 @@ public class AlbumView extends AppCompatActivity {
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
             switch (item.getItemId()) {
-                case R.id.navigation_home:
-                    mTextMessage.setText(R.string.title_home);
+                case R.id.navigation_albums:
+                    mTextMessage.setText(R.string.title_albums);
                     return true;
-                case R.id.navigation_dashboard:
-                    mTextMessage.setText(R.string.title_dashboard);
+                case R.id.navigation_songs:
+                    mTextMessage.setText(R.string.title_songs);
                     return true;
-                case R.id.navigation_notifications:
-                    mTextMessage.setText(R.string.title_notifications);
+                case R.id.navigation_nowPlaying:
+                    mTextMessage.setText(R.string.title_nowPlaying);
                     return true;
             }
             return false;
