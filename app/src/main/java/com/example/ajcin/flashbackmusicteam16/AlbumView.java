a6505e0e926f1f99e7f60f2b8061389b365c8b54
shuo <shc295@ucsd.edu>
2018-02-09 15:02:58 -0800

    add layout of NowPlaying

--- app/src/main/java/com/example/ajcin/flashbackmusicteam16/AlbumView.java
+++ app/src/main/java/com/example/ajcin/flashbackmusicteam16/AlbumView.java
@@ -1,5 +1,7 @@
 package com.example.ajcin.flashbackmusicteam16;
 
+import android.app.FragmentManager;
+import android.app.FragmentTransaction;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
@@ -9,22 +11,21 @@ import android.widget.TextView;
 
 public class AlbumView extends AppCompatActivity {
 
-    private TextView mTextMessage;
 
     private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
             = new BottomNavigationView.OnNavigationItemSelectedListener() {
 
         @Override
         public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            FragmentManager fragmentManager = getFragmentManager();
+            FragmentTransaction transaction =fragmentManager.beginTransaction();
             switch (item.getItemId()) {
                 case R.id.navigation_albums:
-                    mTextMessage.setText(R.string.title_albums);
                     return true;
                 case R.id.navigation_songs:
-                    mTextMessage.setText(R.string.title_songs);
                     return true;
                 case R.id.navigation_nowPlaying:
-                    mTextMessage.setText(R.string.title_nowPlaying);
+                    transaction.replace(R.id.musicItems,new NowPlayingFragment()).commit();
                     return true;
             }
             return false;
@@ -36,7 +37,6 @@ public class AlbumView extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_album_view);
 
-        mTextMessage = (TextView) findViewById(R.id.message);
         BottomNavigationView navigation = (BottomNavigationView) findViewById(R.id.navigation);
         navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
     }
