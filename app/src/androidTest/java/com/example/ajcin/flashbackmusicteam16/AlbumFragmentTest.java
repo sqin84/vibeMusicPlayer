4e4dbfcc7d14cb22f8bf93eafd395b70c3e6416a
shuo <shc295@ucsd.edu>
2018-03-08 12:28:22 -0800

    abc

---
+++ app/src/androidTest/java/com/example/ajcin/flashbackmusicteam16/AlbumFragmentTest.java
@@ -0,0 +1,19 @@
+package com.example.ajcin.flashbackmusicteam16;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by apple on 2/18/18.
+ */
+public class AlbumFragmentTest {
+    @Test
+    public void onCreateView() throws Exception {
+    }
+
+    @Test
+    public void onListItemClick() throws Exception {
+    }
+
+}
---
+++ app/src/androidTest/java/com/example/ajcin/flashbackmusicteam16/albumsongsFragmentTest.java
@@ -0,0 +1,26 @@
+package com.example.ajcin.flashbackmusicteam16;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by apple on 2/18/18.
+ */
+public class albumsongsFragmentTest {
+    @Test
+    public void onCreateView() throws Exception {
+
+    }
+
+    @Test
+    public void onListItemClick() throws Exception {
+
+    }
+
+    @Test
+    public void changeToNowPlaying() throws Exception {
+        
+    }
+
+}
